679323a0ae6cb1b8ad0c0f2ea92c2cf78bc7d656
illumibarti <47025998+illumibarti@users.noreply.github.com>
2021-03-29 13:25:45 +0200

    magiczna wycinka

--- panel3c2.php
+++ panel3c2.php
@@ -7,10 +7,21 @@
         $q1 = "SELECT * FROM `users` WHERE `id`=$id";
         $r1 = mysqli_query($db, $q1);
         $row1 = mysqli_fetch_row($r1);
-        //print_r($row1);
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
         if($row1[5]==2) { //uczen
+?>
 
+<?php
         }
         if($row[5]==1) { //nauczyciel
 
@@ -19,7 +30,11 @@
 
         }
 
-        mysqli_close($db);
+        mysqli_close($db);  
+?>
+</body>
+</html>
+<?php
     } else {
         header("location http://localhost/panel3c2.php?page=login"); 
     }
