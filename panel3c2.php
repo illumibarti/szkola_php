8f18c3e7b7922ba61e9971066737faea2ba6062f
illumibarti <47025998+illumibarti@users.noreply.github.com>
2021-04-12 13:17:31 +0200

    poddalem sie nie chce mi sie pisac

--- panel3c2.php
+++ panel3c2.php
@@ -5,6 +5,29 @@
 
         $id = $_SESSION['id'];
 
+        if(isset($_POST['zmien_haslo_m'])) {
+            $newpass1 = $_POST['newpass1'];
+            $newpass2 = $_POST['newpass2'];
+            if($newpass1 == $newpass2) {
+                $newpass = md5($newpass1);
+                $id_u = $_GET['zmien'];
+                $q8 = "UPDATE `users` SET `password` = '$newpass1' WHERE `id`=$id_u";
+                if(mysqli_query($db,$q8)) echo '<h1>Haslo zostalo zmienione</h1>';
+            } else "<h1>blad baszy danych</h1>";
+        } else echo "<h1>Nowe haslo nie zostalo powtorzoe prawidlowo</h1>";
+    }
+
+    if(isset($_POST['zmien_email_m'])) {
+        $newemail1 = $_POST['newemail1'];
+        $newemail2 = $_POST['newemail2'];
+        if($newemail1 == $newemail2) {
+            $id_u = $_GET['zmien'];
+            $q9 = "UPDATE `users` SET `email`='$newemail1' WHERE `id`=$id_u";
+            if(mysqli_query($db,$q9)) echo "<h1>email zmieniony prawidlowo</h1>";
+            else "<h1>blad bazy danych</h1>";
+        } else echo "<h1>Nowe emaile nie zostaly powtorzone prawidlowo";
+    } 
+
         if(isset($_POST['zmien_haslo'])) {
             $lastpass = $_POST['lastpass'];
             $newpass1 = $_POST['newpass1'];
@@ -79,6 +102,28 @@
 <?php
         }
         if($row[5]==3) { //moderator
+            if(isset($_GET['zmien'])) {
+                $id_u = $_GET['zmien'];
+                $q7 = "SELECT `email` FROM `users` WHERE `id`=$id_u";
+                $r7 = mysqli_query($db, $q7);
+                $row7 = mysqli_fetch_row($r7);
+?>
+                <h2>Zmiana hasla</h2>
+                <form action="" method="post">
+                    <input type="password" name="newpass1" id="">
+                    <input type="password" name="newpass2" id="">
+                    <input type="submit" value="Zmien haslo" name="zmien_haslo_m">
+                </form>
+                <h2>Zmiana adresu email</h2>
+                <form action="" method="post">
+                    <input type="email" name="lastemail" id="" value="<?php echo $row7[0]; ?>" disabled><br>
+                    <input type="email" name="newemail1" id="">
+                    <input type="email" name="newemail2" id="">
+                    <input type="submit" value="Zmien email" name="zmien_email_m">
+                </form> 
+<?php
+            }
+            echo "<h2>Lista uzytkownikow</h2>";
             $q5 = "SELECT `id`, `email`, `id_profile` FROM `users` WHERE `id_profile`<3";
             $r5 = mysqli_query($db, $q5);
             echo '<table>';
@@ -100,7 +145,7 @@
 </body>
 </html>
 <?php
-    } else {
+     } else {
         header("location http://localhost/panel3c2.php?page=login"); 
     }
 ?>
